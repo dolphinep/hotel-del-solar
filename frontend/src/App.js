7948cf06f279de4e6782bf45d9435551ef107000
dolphinep <ping.waragon@gmail.com>
2019-11-11 23:51:21 +0700

    add router 3 pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import './App.css';
-import Nav from './components/NavBar';
+import Nav from './pages/components/NavBar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Admin from './pages/Admin';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Nav/>
-      Hello
-    </div>
+    <Router>
+      <div>
+        <Nav/>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">Registeration</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ frontend/src/pages/Admin.js
@@ -0,0 +1,15 @@
+import React,{ Component } from "react";
+import Table from './components/Table'
+
+class Admin extends Component {
+
+    render(){
+        return(
+            <div>
+                Admin
+                <Table/>
+            </div>
+        )
+    }
+}
+export default Admin;
