af37fa294be09c8c3fa8cae650eb428e1250c670
Porsche <porsche@wariss-MacBook-Pro.local>
2019-11-15 16:59:10 +0700

    Admin login

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,13 +1,57 @@
 import React,{ Component } from "react";
 
-class Admin extends Component {
+const validate = [{id:"p",password: '1234'}];
 
-    render(){
-        return(
+
+class Admin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            password: ' '
+        };
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+   onSubmit = (e) => {
+       e.preventDefault();
+       const form = {
+        password: this.state.password
+       }
+      
+       this.setState({
+            password: ''
+       })
+
+       if(form.password == validate[0].password){
+            alert("success")
+       }
+       else{
+           alert("Fail")
+       }
+    }
+
+    render() {
+        return (
             <div>
-                Admin page
+            <form>
+                <label>
+                    <h1>Password:</h1>
+                    <input 
+                        name='password'
+                        value={this.state.password} 
+                        onChange={e => this.handleChange(e)}/>
+                </label>
+                <button onClick={(e) => this.onSubmit(e)}>SUBMIT</button>         
+            </form>
             </div>
-        )
+        );
     }
 }
+
+
 export default Admin;
